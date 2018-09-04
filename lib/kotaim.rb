185037794f9ec94f298cfb331748a022bd17b0b2
Michael Kang <michael3dk@gmail.com>
2018-09-03 23:48:34 -0700

    fixed rubocop errors

--- lib/kotaim.rb
+++ lib/kotaim.rb
@@ -1,4 +1,6 @@
-require "kotaim/version"
+# frozen_string_literal: true
+
+require 'kotaim/version'
 
 module Kotaim
   # Your code goes here...
--- spec/kotaim_spec.rb
+++ spec/kotaim_spec.rb
@@ -1,9 +1,7 @@
-RSpec.describe Kotaim do
-  it "has a version number" do
-    expect(Kotaim::VERSION).not_to be nil
-  end
+# frozen_string_literal: true
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe Kotaim do
+  it 'has a version number' do
+    expect(Kotaim::VERSION).not_to be nil
   end
 end
